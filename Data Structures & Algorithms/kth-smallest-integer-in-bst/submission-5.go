bb42b3780b55de71e5d407e3a42b211eaff60dfe
Farid Abulias <farid.abulias@gmail.com>
2026-04-13 23:50:46 -0400

    Add: kth-smallest-integer-in-bst - submission-5

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-5.go
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// Recursive DFS
+func kthSmallest(root *TreeNode, k int) int {
+	counter, smallest := k, 0
+	var inOrder func(node *TreeNode)
+	inOrder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inOrder(node.Left)
+		if counter == 0 {
+			return
+		}
+		counter--
+		if counter == 0 {
+			smallest = node.Val
+			return
+		}
+		inOrder(node.Right)
+	}
+	inOrder(root)
+	return smallest
+}
+
+
+// Iterative DFS
+/*
+func kthSmallest(root *TreeNode, k int) int {
+	n := 0
+	stack := []*TreeNode{}
+	curr := root
+
+	for curr != nil || len(stack) > 0 {
+		for curr != nil {
+			stack = append(stack, curr)
+			curr = curr.Left
+		}
+		curr = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		n++
+		if n == k {
+			return curr.Val
+		}
+		curr = curr.Right
+	}
+	return -1
+}
+*/
