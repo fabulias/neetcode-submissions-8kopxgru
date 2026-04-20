843ffcb193ba701e9a9e5cea9fa45aa74e9d05e0
Farid Abulias <farid.abulias@gmail.com>
2026-04-19 20:11:10 -0400

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.go
@@ -0,0 +1,27 @@
+// top-down approach
+func climbStairs(n int) int {
+	dp := make([]int, n)
+	for ix := range n {
+		dp[ix] = -1
+	}
+    var dfs func(step int) int
+	dfs = func(step int) int {
+		if step >= n {
+			if step == n {
+				return 1
+			}
+			return 0
+		}
+
+		if dp[step] != -1 {
+			return dp[step]
+		}
+
+		dp[step] = dfs(step+1) + dfs(step+2)
+		return dp[step]
+	}
+	
+	return dfs(0)
+}
+
+// bottom-up approach
